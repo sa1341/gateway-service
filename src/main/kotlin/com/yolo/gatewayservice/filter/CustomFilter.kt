9abfa599e634eaea007cce904d80c6a65c0a68aa
임준영 <a79007714@gmail.com>
2022-07-02 02:28:11 +0900

    feat: GW CustomFilter 적용

---
+++ src/main/kotlin/com/yolo/gatewayservice/filter/CustomFilter.kt
@@ -0,0 +1,43 @@
+package com.yolo.gatewayservice.filter
+
+import org.slf4j.LoggerFactory
+import org.springframework.cloud.gateway.filter.GatewayFilter
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+
+@Component
+class CustomFilter: AbstractGatewayFilterFactory<CustomFilter.Config>(Config::class.java) {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    override fun apply(config: Config): GatewayFilter {
+        return GatewayFilter { exchange, chain ->
+            val request = exchange.request
+            val response = exchange.response
+            chain.filter(exchange).then(Mono.fromRunnable { log.info("CustomFilter response status code: ${response.statusCode}") })
+        }
+    }
+
+    class Config
+}
+
+@Component
+class GlobalFilter : AbstractGatewayFilterFactory<GlobalFilter.Config>(Config::class.java) {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    override fun apply(config: Config): GatewayFilter {
+        return GatewayFilter { exchange, chain ->
+            val request = exchange.request
+            val response = exchange.response
+
+            log.info("Global request id: ${request.id}")
+            chain.filter(exchange).then(Mono.fromRunnable {
+                log.info("Global response status code: ${response.statusCode}")
+            })
+        }
+    }
+
+    class Config
+}
